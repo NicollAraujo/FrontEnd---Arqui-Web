2a151bbc6670c43cb866785ce8dcd1e25bf3e8cd
Alvaro0711 <u20171f342@upc.edu.pe>
2023-11-08 00:17:44 -0500

    Beta 8-11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { authGuard } from './guard/auth.guard';
-import { PanelComponent } from './components/panel/panel.component';
-import { RolComponent } from './components/rol/rol.component';
-import { CreaeditaAreatrabajoComponent } from './components/areatrabajo/creaedita-areatrabajo/creaedita-areatrabajo.component';
-import { CreaeditaRolComponent } from './components/rol/creaedita-rol/creaedita-rol.component';
+import { GpComponent} from './components/GrupoDeProyecto/gp/gp.component';
+import { CreaeditaGpComponent} from './components/GrupoDeProyecto/creaedita-gp/creaedita-gp.component';
+import { ComunicadoComponent } from './components/Comunicado/comunicado/comunicado.component';
+import { CreaeditaComunicadoComponent } from './components/Comunicado/creaedita-comunicado/creaedita-comunicado.component';
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent},
-  {path:'login',component:LoginComponent},
-  {path:'panel',component:PanelComponent,canActivate:[authGuard]},
   {
-    path: 'roles',
-    component: RolComponent,
+    path: 'grupos',
+    component: GpComponent,
     children: [
-      { path: 'nuevo', component: CreaeditaRolComponent },
-      { path: 'ediciones/:id', component: CreaeditaRolComponent },
+      { path: 'nuevo', component: CreaeditaGpComponent},
+      { path: 'ediciones/:id', component: CreaeditaGpComponent },
     ],
   },
-  {path:'**',pathMatch:'full',redirectTo:'home'}
+
+  {
+    path: 'comunicados',
+    component: ComunicadoComponent,
+    children: [
+      { path: 'nuevo', component: CreaeditaComunicadoComponent},
+      { path: 'ediciones/:id', component: CreaeditaComunicadoComponent },
+    ],
+  },
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,73 +1,57 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
-import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
-import { PanelComponent } from './components/panel/panel.component';
-import { RolComponent } from './components/rol/rol.component';
-import { ListarRolComponent } from './components/rol/listar-rol/listar-rol.component';
-import { CreaeditaRolComponent } from './components/rol/creaedita-rol/creaedita-rol.component';
-import { AreatrabajoComponent } from './components/areatrabajo/areatrabajo.component';
-import { ListarAreatrabajoComponent } from './components/areatrabajo/listar-areatrabajo/listar-areatrabajo.component';
-import { CreaeditaAreatrabajoComponent } from './components/areatrabajo/creaedita-areatrabajo/creaedita-areatrabajo.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { GpComponent } from './components/GrupoDeProyecto/gp/gp.component';
+import { ListarGpComponent } from './components/GrupoDeProyecto/listar-gp/listar-gp.component';
+import { CreaeditaGpComponent } from './components/GrupoDeProyecto/creaedita-gp/creaedita-gp.component';
+import { ComunicadoComponent } from './components/Comunicado/comunicado/comunicado.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
-import { AuthInterceptor } from './interceptor';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListarComunicadoComponent } from './components/Comunicado/listar-comunicado/listar-comunicado.component';
+import { CreaeditaComunicadoComponent } from './components/Comunicado/creaedita-comunicado/creaedita-comunicado.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LoginComponent,
-    PanelComponent,
-    RolComponent,
-    ListarRolComponent,
-    CreaeditaRolComponent,
-    AreatrabajoComponent,
-    ListarAreatrabajoComponent,
-    CreaeditaAreatrabajoComponent,
-    NavbarComponent,
+    GpComponent,
+    ListarGpComponent,
+    CreaeditaGpComponent,
+    ComunicadoComponent,
+    ListarComunicadoComponent,
+    CreaeditaComunicadoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
+    AppRoutingModule
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
     MatButtonModule,
     MatInputModule,
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatPaginatorModule,
-    MatMenuModule,
     MatIconModule,
     MatToolbarModule,
-    MatTableModule,
+    MatMenuModule,
+    FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
   ],
-  providers: [
-    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
-    JwtHelperService,
-    {provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,}
-  ],
-  bootstrap: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
