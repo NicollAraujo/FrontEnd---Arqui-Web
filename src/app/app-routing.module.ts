d66c8cf8d923faf3d948f0de3956191fe07d1c40
Alvaro0711 <u20171f342@upc.edu.pe>
2023-11-07 20:57:39 -0500

    Se modifico y agrego el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,37 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './guard/auth.guard';
 import { PanelComponent } from './components/panel/panel.component';
+import { GpComponent } from './components/GrupoDeProyecto/gp/gp.component';
+import { CreaeditaGpComponent } from './components/GrupoDeProyecto/creaedita-gp/creaedita-gp.component';
+import { ComunicadoComponent } from './components/Comunicado/comunicado/comunicado.component';
+import { CreaeditaComunicadoComponent } from './components/Comunicado/creaedita-comunicado/creaedita-comunicado.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'panel',component:PanelComponent,canActivate:[authGuard]},
   {path:'**',pathMatch:'full',redirectTo:'home'}
+];
+
+const routes: Routes = [
+  {
+    path: 'grupos',
+    component: GpComponent,
+    children: [
+      { path: 'nuevo', component: CreaeditaGpComponent},
+      { path: 'ediciones/:id', component: CreaeditaGpComponent },
+    ],
+  },
+
+  {
+    path: 'comunicados',
+    component: ComunicadoComponent,
+    children: [
+      { path: 'nuevo', component: CreaeditaComunicadoComponent},
+      { path: 'ediciones/:id', component: CreaeditaComunicadoComponent },
+    ],
+  },
+
 ];
 
 @NgModule({
